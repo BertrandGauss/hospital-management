06af0b340515c9927d7c1468ecde38adb92abbaf
BertrandGauss <673809859@qq.com>
2021-12-27 20:54:54 +0800

    add patient register

--- src/main/java/com/hospital/MainApplication.java
+++ src/main/java/com/hospital/MainApplication.java
@@ -1,4 +1,17 @@
 package com.hospital;
 
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+@ServletComponentScan
+
+@MapperScan("com.hospital.mapper")
 public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class);
+    }
 }
---
+++ src/main/java/com/hospital/mapper/PatientMapper.java
@@ -0,0 +1,10 @@
+package com.hospital.mapper;
+
+
+import com.hospital.entity.Patient;
+
+public interface PatientMapper {
+    void add(Patient patient);
+    Integer selectByIdentificationNum(String pIdentificationNum);
+    Integer selectidbyphone(String pPhone);
+}
